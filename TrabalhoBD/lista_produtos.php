4bf3e90610839df3342914d375615b37604a3368
Mattheus Soares Santos <tz_santos@hotmail.com>
2017-07-04 21:59:45 -0300

    Add files via upload

--- TrabalhoBD/lista_produtos.php
+++ TrabalhoBD/lista_produtos.php
@@ -11,23 +11,22 @@
 
 
 <?php 
-	session_start();
-	$codigo_login = $_SESSION['codigo_login'];
-
 	$link = mysqli_connect("localhost", "root", "", "trab");
-	$query = "SELECT nome, descricao, preco_venda	 FROM produto ";
+	$query = "SELECT nome, descricao, preco_venda, codigo	 FROM produto ";
 	$consultaSQL = mysqli_query($link,$query);
 
-	echo "<table border='1'>
+	echo "<table border='1' align='center'>
 		<tr align='center'>	
 			<td>nome</td>
 			<td>descrição</td>
-			<td>valor sugerido</td>
+			<td>preço</td>
 		</tr>";
 	while($row = mysqli_fetch_row($consultaSQL)){
 		echo "<tr>";
 		for($i=0; $i<mysqli_num_fields($consultaSQL); $i++){
-			echo "<td>".$row[$i]."</td>";
+			if(($i%4)-3!=0) echo "<td>".$row[$i]."</td>";
+			if(($i%4)-3==0) echo "<td> <a href=produto.php?codigo=".$row[$i].">Comprar</a></td>";
+
 		}
 		echo "</tr>";
 	}
---
+++ TrabalhoBD/produto.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Trabalho de Banco de Dados</title>
+
+<script type="text/javascript" src="JS/script.js"></script>
+
+</head>
+<body> 
+
+
+<?php
+	
+	$codigo_produto=$_GET['codigo'];
+
+	$link = mysqli_connect("localhost", "root", "", "trab");
+	
+	mysqli_close($link);
+?>
+
+</body>
+</html>
